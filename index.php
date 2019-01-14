dc726702c090a79fee716b5be0048430ecedf83f
Simon Oulevay <simon.oulevay@gmail.com>
2019-01-14 21:11:34 +0100

    Fix github ribbon

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ $name = $_GET['name'] ?: 'World';
 
   <body>
 
-    <a href="https://github.com/MediaComem/heroku-php-hello-world">
+    <a href="https://github.com/MediaComem/php-hello-world-form">
       <img style="position: absolute; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png" alt="Fork me on GitHub">
     </a>
 
